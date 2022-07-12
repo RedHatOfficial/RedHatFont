0bcff22295d6ca466141c1ed673f1dc3f152b993
Wes Ruvalcaba <wesruv@gmail.com>
2022-07-12 14:50:09 -0500

    Copied over more files from jeremymickel and rebuilt, committing everything

--- mastering/gen_stat_text.py
+++ mastering/gen_stat_text.py
@@ -9,12 +9,11 @@ UPRIGHT_AXES = [
         name="Weight",
         ordering=0,
         values=[
-            dict(nominalValue=300, rangeMinValue=200, rangeMaxValue=350, name="Light"),
-            dict(nominalValue=400, rangeMinValue=350, rangeMaxValue=400, name="Regular", flags=0x2, linkedValue=700),
-            dict(nominalValue=500, rangeMinValue=450, rangeMaxValue=650, name="Medium"),
-            dict(nominalValue=700, rangeMinValue=650, rangeMaxValue=850, name="Bold"),
-            dict(nominalValue=900, rangeMinValue=850, rangeMaxValue=950, name="Black"),
-            # dict(value=400, name="Regular", flags=0x2, linkedValue=700),  # Regular
+            dict(value=300, name="Light"),
+            dict(value=400, name="Regular", flags=0x2, linkedValue=700,),
+            dict(value=500, name="Medium"),
+            dict(value=600, name="SemiBold"),
+            dict(value=700, name="Bold"),
         ],
     ),
     dict(
@@ -33,12 +32,11 @@ ITALIC_AXES = [
         name="Weight",
         ordering=0,
         values=[
-            dict(nominalValue=300, rangeMinValue=200, rangeMaxValue=350, name="Light"),
-            dict(nominalValue=400, rangeMinValue=350, rangeMaxValue=400, name="Regular", flags=0x2, linkedValue=700),
-            dict(nominalValue=500, rangeMinValue=450, rangeMaxValue=650, name="Medium"),
-            dict(nominalValue=700, rangeMinValue=650, rangeMaxValue=850, name="Bold"),
-            dict(nominalValue=900, rangeMinValue=850, rangeMaxValue=950, name="Black"),
-            # dict(value=400, name="Regular", flags=0x2, linkedValue=700),  # Regular
+            dict(value=300, name="Light"),
+            dict(value=400, name="Regular", flags=0x2, linkedValue=700,),
+            dict(value=500, name="Medium"),
+            dict(value=600, name="SemiBold"),
+            dict(value=700, name="Bold"),
         ],
     ),
     dict(
@@ -51,8 +49,8 @@ ITALIC_AXES = [
     ),
 ]
 
-UPRIGHT_SRC = f"./fonts/proportional/RedHatTextVF[wght].ttf"
-ITALIC_SRC = f"./fonts/proportional/RedHatTextVF-Italic[wght].ttf"
+UPRIGHT_SRC = f"./fonts/proportional/RedHatText[wght].ttf"
+ITALIC_SRC = f"./fonts/proportional/RedHatText-Italic[wght].ttf"
 
 def update_fvar(ttfont):
     fvar = ttfont['fvar']
